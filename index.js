c809c20c10f64aaafa52026cfb2c4f7ec60a4829
estea8968 <estea8968@gmail.com>
2022-03-08 08:30:10 +0800

    增加index.html index.js

---
+++ index.js
@@ -0,0 +1,183 @@
+let user_name ='';
+let endrow =2773;
+let all_data = null;
+let json_data = null;
+let search_result = null;
+let now_people = null;
+let sun ;
+//show_people [th15_1[太],th16_2[祖],th17_3[志],th18_4[存],th19_5[武],th20_6[達],th21_7[國],th22_8[朝],th23_9[宗],th24_10[崇]]
+const sh_ary = ['15世','16世','17世','18世','19世','20世','21世','22世','23世','24世','25世']
+let show_people = {};
+
+const url ='https://docs.google.com/spreadsheets/d/1hre_XZDiFvVskOC-_NT8s0gNiTcqpfd1hkOZRHr5Wuo/edit#gid=1817346169';
+const sheet_tag ='族籍';
+get_all_data();
+let show_name_id = document.getElementById('show_name');
+let show_all_data_id = document.getElementById('show_all_data');
+
+//隱藏id
+show_name_id.style.display = 'none';
+show_all_data_id.style.display = 'block';
+
+
+//搜尋json
+function json_search(obj,key,value) {
+  //console.log(array);
+  var obj = JSON.parse(obj);
+  //console.log(obj.length);
+  var back_data = [];
+  for(var i=0;i<obj.length;i++){
+      if(obj[i][key] == value){
+          //return obj[i];
+          back_data.push(obj[i]);
+          console.log(obj[i]);
+      }
+  }
+  return back_data;
+  //console.log(results);
+  //return results;
+}
+
+//取出輸入值
+async function getValueInput(){
+    user_name =document.getElementById('user_name').value
+    //document.getElementById("show_name").innerHTML = user_name;
+    //顯示id
+    show_name_id.style.display = 'block';
+    show_all_data_id.style.display = 'none';
+    //console.log('title',json_data[0][2]);
+    //找本人
+    search_result = json_search(json_data,"名",user_name);
+    if(search_result.length==1){
+            //console.log(search_result.族籍號);
+            //console.log(search_result.族籍號.length) ;
+            var search_result0 =search_result[0];    
+            var a_people = null;
+            var i;
+            var show_data =user_name+'族籍：';
+            for (i = 1; i <= search_result0.族籍號.length; i++) {    
+                //a_people = await build_show_people(search_result.族籍號,i);
+                const people_number = search_result0.族籍號.substr(0,i);
+                a_people = json_search(json_data,"族籍號",people_number);
+                if(i==search_result0.族籍號.length){
+                    show_data = show_data + `<div><button class="btn_self" onclick="search_byid('${a_people[0].族籍號}')">${sh_ary[i-1]}${a_people[0].名}${a_people[0].族籍號}<br>/配偶:${a_people[0].配偶}/父親:${a_people[0].父親}</button></div><p></p>`;
+                }else{
+                    show_data = show_data + `<div><button onclick="search_byid('${a_people[0].族籍號}')">${sh_ary[i-1]}${a_people[0].名}${a_people[0].族籍號}<br>/配偶:${a_people[0].配偶}/父親:${a_people[0].父親}</button></div><p></p>`;
+                }
+                
+                console.log(a_people);
+            }
+            //search sun
+            search_sun(a_people[0].族籍號);
+            //console.log(sun);
+            if(sun.length>0){
+                for(var x=0;x<sun.length;x++){
+                    show_data = show_data + `<div><button class="btn_sun" onclick="search_byid('${sun[x].族籍號}')">${sh_ary[i-1]}${sun[x].名}${sun[x].族籍號}<br>配偶:${sun[x].配偶}/父親:${sun[x].父親}</button></div>`;
+                }
+            }
+    }else{
+        var a_people = null;
+        var i;
+        var show_data ='搜尋到多筆'+user_name;
+        search_result.forEach(function(value) {
+            show_data = show_data + `<div><button class="btn_seach" onclick="search_byid('${value.族籍號}')">${value.族籍號}/${value.名}<br>配偶:${value.配偶}/父親:${value.父親}</button></div><p></p>`;
+        });
+    }
+    document.getElementById("show_name").innerHTML = show_data;
+    show_name_id.style.display = 'block';
+    show_all_data_id.style.display = 'none';
+     
+}
+async function search_sun(id){
+    var r_people = [];
+    var sun_id ='';
+    for(var i=1;i<10;i++){
+        sun_id = id+i.toString();
+        a_people = json_search(json_data,"族籍號",sun_id);
+        if(a_people.length>0){
+            r_people.push(a_people[0]);
+        }
+    }
+    console.log(r_people);
+    sun = r_people;
+    //return r_people;
+
+}
+
+async function search_byid(id){
+    var a_people = null;
+        var i;
+        var show_data ='族籍序：';
+            for (i = 1; i <= id.length; i++) {    
+                //a_people = await build_show_people(search_result.族籍號,i);
+                const people_number = id.substr(0,i);
+                a_people = json_search(json_data,"族籍號",people_number);
+                if(i == id.length){
+                    show_data = show_data + `<div><button class="btn_self" onclick="search_byid('${a_people[0].族籍號}')">${sh_ary[i-1]}${a_people[0].族籍號}/名:${a_people[0].名}<br>配偶:${a_people[0].配偶}/父親:${a_people[0].父親}</button></div><p></p>`;
+                }else{
+                    show_data = show_data + `<div><button onclick="search_byid('${a_people[0].族籍號}')">${sh_ary[i-1]}${a_people[0].族籍號}/名:${a_people[0].名}<br>配偶:${a_people[0].配偶}/父親:${a_people[0].父親}</button></div><p></p>`;
+                }
+                console.log(a_people);
+            }
+            //search sun
+            search_sun(a_people[0].族籍號);
+            //console.log(sun);
+            if(sun.length>0){
+                for(var x=0;x<sun.length;x++){
+                    show_data = show_data+`<div><button class="btn_sun" onclick="search_byid('${sun[x].族籍號}')">${sh_ary[i-1]}${sun[x].名}族籍號:${sun[x].族籍號}<br>配偶:${sun[x].配偶}/父親:${sun[x].父親}</button></div>`;
+                }
+            }
+            document.getElementById("show_name").innerHTML = show_data;
+            show_name_id.style.display = 'block';
+            show_all_data_id.style.display = 'none';    
+}
+
+function get_all_data(){
+    var $show = $('#show_all_data');
+    var a = {
+        sheetUrl : url,
+        sheetTag : sheet_tag,
+        row: 1,
+        col: 1,
+        endRow : endrow,
+        endCol : 6
+    };
+    $.get('https://script.google.com/macros/s/AKfycbzBZXaA2Gf9-6gW0Whm-zbczf0bs6dIAk0FMyCpi7xItwMVyRRdD3koKRtZmoSeNg_MHQ/exec',a, function(data){
+      var d = data.split(',');
+      var arr = [];
+      $show.append('<table>'); 
+      for(var i=0; i<(a.endRow-a.row+1); i++){
+      //  for(var i=0; i<d.length; i++){  
+        arr[i] = d.splice(0, (a.endCol-a.col+1)); 
+        //console.log(arr[i]);
+        var show_str ='<tr>';
+        for(var x=0;x<arr[i].length;x++){
+            show_str = show_str +'<td>'+arr[i][x]+'</td>';
+        }
+        show_str = show_str +'</tr>';
+        $show.append(show_str);
+        //$show.append(arr[i]+'<br/>');
+      }
+      $show.append('</table>'); 
+      all_data = arr; 
+      json_data = datatoJSON(all_data) ;
+    });
+ 
+}
+
+function datatoJSON() {
+    var result = [];
+    var headers = all_data[0];
+    for (var i = 1; i < all_data.length; i++) {
+        var obj = {};
+        var currentline = all_data[i];
+
+        for (var j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+        }
+        result.push(obj);
+    }
+    //return result; //JavaScript object
+    console.log(result);
+    return JSON.stringify(result); //JSON
+}
